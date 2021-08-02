839dda9c54114aed3e71f987673501c103d03719
Brad <3398066+ItsNatoriousB@users.noreply.github.com>
2021-08-02 08:37:02 -0400

    Update bocw-mats.py

--- bocw-mats.py
+++ bocw-mats.py
@@ -1,4 +1,4 @@
-# Configuration for Call Of Duty Warzone
+# Configuration for Black Ops Cold War
 # Texture map types will be defined here
 
 import bpy
@@ -23,6 +23,10 @@ ApplyMaterialToCurrentlySelectedObject = False
 # Input Configuration End
 # DO NOT TOUCH ANY CODE BELOW
 
+# Open and Read Materials
+with open('readme.txt') as f:
+    lines = f.readlines()
+
 # Material Creation
 if not outputMaterialName:
     outputMaterialName = os.path.basename(DotMatPath)
@@ -46,8 +50,8 @@ textureBasePath = os.path.dirname(DotMatPath)
 with open(DotMatPath) as f:
     for l in f:
         line = l.rstrip()
-        if line.startswith("Diffuse="):
-            diffuseImgPath = textureBasePath + r"/" + line.replace("Diffuse=", "") + ".tga"
+        if line.startswith("colorMap"):
+            diffuseImgPath = textureBasePath + r"/" + line.replace("colorMap", "") + ".png"
             print(diffuseImgPath)
             #Diffuse Texture
             diffuseTex = mat.node_tree.nodes.new("ShaderNodeTexImage")
@@ -56,11 +60,12 @@ with open(DotMatPath) as f:
             diffuseTex.location = Vector((-400,450))
             #diffuseTex.hide = True
 
-            #Connect diffuse texture to 'PrincipleBSDF'
+            #Connect Diffuse to 'PrincipleBSDF'
             mat.node_tree.links.new(diffuseTex.outputs[0], principleBSDF.inputs[0])
             #Diffuse End
-        elif line.startswith("Normal="):
-            normalImgPath = textureBasePath + r"/" + line.replace("Normal=", "") + ".tga"
+
+        elif line.startswith("normalMap"):
+            normalImgPath = textureBasePath + r"/" + line.replace("normalMap", "") + ".png"
             print(normalImgPath)
             
             #Normal Start
@@ -88,8 +93,9 @@ with open(DotMatPath) as f:
             mat.node_tree.links.new(normCurve.outputs[0], normMap.inputs[1])
             mat.node_tree.links.new(normMap.outputs[0], principleBSDF.inputs['Normal'])
             #Normal End
+
         elif line.startswith("Specular="):
-            specularImgPath = textureBasePath + r"/" + line.replace("Specular=", "") + ".tga"
+            specularImgPath = textureBasePath + r"/" + line.replace("Specular=", "") + ".png"
             print (specularImgPath)
             
             #Specular Start
@@ -106,15 +112,16 @@ with open(DotMatPath) as f:
             specTex.location = Vector((-600, specY))
             #specTex.hide = True
 
-            #Connect specular texture to 'Seperate RGB'
+            #Connect Specular to 'Seperate RGB'
             mat.node_tree.links.new(specTex.outputs[0], specSeperateRGB.inputs[0])
             #Connect 'Seperate RGB' to 'PrincipleBSDF'
             mat.node_tree.links.new(specSeperateRGB.outputs[0], principleBSDF.inputs['Specular'])
             mat.node_tree.links.new(specSeperateRGB.outputs[1], principleBSDF.inputs['Metallic'])
             mat.node_tree.links.new(specSeperateRGB.outputs[2], principleBSDF.inputs['Roughness'])
             #Specular End
+
         elif line.startswith("Emissive="):
-            emissiveImgPath = textureBasePath + r"/" + line.replace("Emissive=", "") + ".tga"
+            emissiveImgPath = textureBasePath + r"/" + line.replace("Emissive=", "") + ".png"
             print (emissiveImgPath)
             #Emission Start
             emiTex = mat.node_tree.nodes.new("ShaderNodeTexImage")
@@ -127,7 +134,40 @@ with open(DotMatPath) as f:
             #Connecting
             mat.node_tree.links.new(emiTex.outputs[0], emiShader.inputs[0])
             mat.node_tree.links.new(emiShader.outputs[0], addShader.inputs[1])
-            #Emission End           
+            #Emission End
+
+        elif line.startswith("aoMap"):
+            emissiveImgPath = textureBasePath + r"/" + line.replace("aoMap", "") + ".png"
+            print (aoImgPath)
+            #Ambient Occlusion Start
+            aoTex = mat.node_tree.nodes.new("ShaderNodeTexImage")
+            aoShader = mat.node_tree.nodes.new("ShaderNodeEmission")
+            aoImage = bpy.data.images.load(filepath = aoImgPath)
+            aoTex.image = aoImage
+            #Ambient Occlusion - location
+            aoTex.location = Vector((-200, -425))
+            aoShader.location = Vector((100, -425))
+            #AO Node Connection
+            mat.node_tree.links.new(aoTex.outputs[0], emiShader.inputs[0])
+            mat.node_tree.links.new(aoShader.outputs[0], addShader.inputs[1])
+            #Ambient Occlusion End
+
+
+        elif line.startswith("Gloss="):
+            glossImgPath = textureBasePath + r"/" + line.replace("Gloss=", "") + ".png"
+            print (glossImgPath)
+            #Gloss/Roughness Start
+            glossTex = mat.node_tree.nodes.new("ShaderNodeTexImage")
+            glossShader = mat.node_tree.nodes.new("ShaderNodeEmission")
+            glossImage = bpy.data.images.load(filepath = glossImgPath)
+            glossTex.image = glossImage
+            #Roughness - location
+            glossTex.location = Vector((-200, -425))
+            glossShader.location = Vector((100, -425))
+            #Gloss Node Connection
+            mat.node_tree.links.new(glossTex.outputs[0], glossShader.inputs[0])
+            mat.node_tree.links.new(glossShader.outputs[0], addShader.inputs[1])
+            #Gloss End
 
             
            #END
